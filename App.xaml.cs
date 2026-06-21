02e80edbf7d15fd55bdf9402c9c80b4f10864bfb
Narex56464 <n3gativen@yandex.ru>
2026-06-21 21:03:34 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfApp1.Model;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();
+        }
+    }
+
+}
---
+++ Model/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public string RoleName {  get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
